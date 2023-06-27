7b8ad375f6c2c7cf3b5a7f3152c4677df7cd0a59
MaximTBW <135816371+MaximTBW@users.noreply.github.com>
2023-06-27 20:09:17 +0300

    RuVersion
    Добавил синтезирование речи; добавил nltk, что позволило учесть ошибки и выполнять действия, привязанные к шаблона при разности между сказанным текстом и шаблоном в 5%

--- VoiceBot_SpeechToText.py
+++ VoiceBot_SpeechToText.py
@@ -2,6 +2,16 @@ import speech_recognition as sr
 import subprocess
 import re
 import json
+import os
+import nltk
+import pyttsx3 # использование внетреннего голосового помощника Windows
+engine = pyttsx3.init() 
+voices = engine.getProperty('voices')
+ru_voice_id = voices[0].id # выбор русской Ирины в стандартной Windows 10
+engine.setProperty('voice', ru_voice_id) 
+
+def matching(text,withtext):
+    return nltk.edit_distance(text.lower(),withtext.lower())/min(len(text),len(withtext))
 
 """
 Выгрузка информации с файла в виде
@@ -9,7 +19,7 @@ import json
 """
 with open("Information.json","r") as f:
     variants = json.load(f)
-    
+
 """
 Инициализация обработчика и микрофона
 """
@@ -53,20 +63,46 @@ while 1==1:
         print('Match witch cases of json...')
         for key in variants:
             print("  "+key)
-            try:
-                if(re.search(key.lower(), text.lower()).group() == text.lower()):
-                    print('Run process')
-                    subprocess.run(variants[key])
-                    break
-                """
-                Могут вылетать ошибки несоответствия выражения,
-                необработанного голоса и прочее по мелочи. Можно сделать без
-                except-ов, но надо-ли?
-                """
-            except Exception as e:
-                pass
+            matched = re.search(key.lower(), text.lower())
+            """
+            Проверка на существование шаблона и
+            на совпадение текста с шаблоном с погрешностью в 5%
+            """
+            if(matched!=None and matching(matched.group(),text.lower())<0.05):
+                if(variants[key][0] == "subproc"):
+                    print(f'Run subprocess {variants[key][1]}')
+                    engine.say('запуск процесса')
+                    engine.runAndWait()
+                    subprocess.run(variants[key][1])
+                elif(variants[key][0] == "gensay"):
+                    engine.say(variants[key][1])
+                    engine.runAndWait()
+                elif(variants[key][0] == "start"):
+                    print(f'Start program {variants[key][2]}')
+                    engine.say('запуск программы')
+                    engine.runAndWait()
+                    os.startfile(variants[key][1]+variants[key][2])
+                elif(variants[key][0] == "startparam"):
+                    print(f'Start program {variants[key][2]} with params {variants[key][3]}')
+                    engine.say('запуск программы с параметром')
+                    engine.runAndWait()
+                    os.system(f'start '+(f'/d "{variants[key][1]}" ' if len(variants[key][1])>0 else '')+f'{variants[key][2]} {variants[key][3]}')
+                elif(variants[key][0] == "comand"):
+                    engine.say('запуск команды')
+                    engine.runAndWait()
+                    print(f'Run comand {variants[key][1]}')
+                    os.system(variants[key][1])
+                else:
+                    print(matched.group())
+                
+                break
+            """
+            Могут вылетать ошибки необработанного голоса и прочее по мелочи. Можно сделать без
+            except-ов, но надо-ли?
+            # Надо .-.
+            """
     except Exception as e:
-        pass
+        print(e)
     
 
 
